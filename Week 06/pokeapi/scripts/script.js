0d4ac000c5257e0e16276119df5fe1f6e47f6490
Angel E. Manzano <angel.manzano777@hotmail.com>
2023-02-18 01:53:12 +0000

    Pokemon website API project

--- Week 06/pokeapi/scripts/script.js
+++ Week 06/pokeapi/scripts/script.js
@@ -1,3 +1,49 @@
-date = new date();
+const pokemonContainer = document.querySelector(".pokemon-container");
 
-console.log(date)
+
+//This function fetches pokemon based on ID. 
+function fetchPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then (res => res.json())
+    .then (data => {
+        createPokemon(data);
+    });
+}
+
+
+//This function allows us to enter the number of Pokemon we want to see, and return it up to that number. 
+function fetchPokemons(number) {
+    for (let i = 1; i <= number; i++) {
+        fetchPokemon(i);
+    }
+}
+
+function createPokemon(pokemon){
+    const card = document.createElement('div');
+    card.classList.add('pokemon-block');
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+
+    const sprite = document.createElement('img');
+    sprite.src = pokemon.sprites.front_default;
+
+    imgContainer.appendChild(sprite);
+
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+
+    const name = document.createElement('p');
+    name.classList.add('name');
+    name.textContent = pokemon.name
+
+    card.appendChild(imgContainer);
+    card.appendChild(number);
+    card.appendChild(name);
+
+    pokemonContainer.appendChild(card);
+
+}
+
+
+fetchPokemons(15);
